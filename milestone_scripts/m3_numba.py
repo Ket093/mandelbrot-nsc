4e47bdd6128444d2132739aa6f2864fc4b449f3c
Keto93 <ketawahatengong@gmail.com>
2026-03-02 01:14:22 -0800

    MP1 M3: Verify Numba installation

--- milestone_scripts/m3_numba.py
+++ milestone_scripts/m3_numba.py
@@ -4,17 +4,7 @@ Author: Akondeng Atengong Ketawah
 Course: Numerical Scientific Computing 2026
 """
 
-import time
-import statistics
-import numpy as np
-from numba import njit  # <-- The magic Numba compiler
-import sys
-import os
-
-# Add parent directory for mandelbrot.py import
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# Import functions from mandelbrot.py file
-from mandelbrot import compute_mandelbrot, numpy_mandelbrot
-
-print("M3: Setup complete - ready to implement Numba function")
+# Check if Numba is installed
+import numba
+print(f"Numba version: {numba.__version__}")
+print("Numba is ready to use!")
