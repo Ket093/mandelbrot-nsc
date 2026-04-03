846cb2f0a67b56110abbf1ef9d9e7e77aa345ead
Keto93 <ketawahatengong@gmail.com>
2026-04-02 17:44:10 -0700

    MP2 (Dask cluster): Modified setup.py file - replaced the command with correct python code

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-echo 'from setuptools import setup
+from setuptools import setup
 
 setup(name="mandelbrot_parallel",
       version="0.1",
       py_modules=["mandelbrot_parallel"],
-)' > setup.py
+)
